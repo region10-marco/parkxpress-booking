73ccdd59cc8ebb31e2eb099284421d2ff435f6d5
region10-marco <marco.hinkelmann@region10.group>
2025-03-09 20:33:14 +0100

    Aktualisiere AJAX-Funktion zur Abfrage von Parkplatztypen und verbessere die Fehlerbehandlung

--- ajax/px_get_slots_from_db.php
+++ ajax/px_get_slots_from_db.php
@@ -1,22 +1,18 @@
-<?php
-
+<?php 
 
 add_action('wp_ajax_px_get_parking_types', 'px_get_parking_types');
 add_action('wp_ajax_nopriv_px_get_parking_types', 'px_get_parking_types');
 
 function px_get_parking_types()
 {
-
-
     if (!isset($_POST['check_in']) || !isset($_POST['check_out'])) {
-        echo json_encode(["error" => "Fehlende Parameter"]);
+        wp_send_json_error(["error" => "Fehlende Parameter"]);
         wp_die();
     }
 
-    $db = new PxBookingDB();
-    $data = $db->getParkingTypes(sanitize_text_field($_POST['check_in']), sanitize_text_field($_POST['check_out']));
+    $pxdb = new PxBookingDB();
+    $response = $pxdb->px_get_slot_informations($_POST['check_in'], $_POST['check_out']);
 
-    echo json_encode($data);
+    wp_send_json_success($response);
     wp_die();
 }
-?>
