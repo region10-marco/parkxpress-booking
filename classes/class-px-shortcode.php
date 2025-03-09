defac1b4290cd293f0c5851551a9be005467bd58
region10-marco <marco.hinkelmann@region10.group>
2025-03-09 16:41:47 +0100

    Füge Sitzungsverwaltung hinzu und lade das entsprechende Template basierend auf dem Buchungsfortschritt

--- classes/class-px-shortcode.php
+++ classes/class-px-shortcode.php
@@ -5,14 +5,27 @@ class PxShortcode
     public function __construct()
     {
         add_shortcode('px_buchungstool', array($this, 'px_buchungstool_shortcode'));
+        add_action('init', array($this, 'start_session'));
+    }
+
+    public function start_session()
+    {
+        if (!session_id()) {
+            session_start();
+        }
     }
 
     public function px_buchungstool_shortcode($atts)
     {
         ob_start();
-        // Hier kannst du deine Klassen und Logik einbinden, z.B.:
         include dirname(plugin_dir_path(__FILE__)) . '/lang/order_lang_de.php';
-        include dirname(plugin_dir_path(__FILE__)) . '/templates/step-one.php';
+
+        // Prüfen, ob Step One abgeschlossen ist und Step Two geladen werden soll
+        if (isset($_SESSION['buchung_step']) && $_SESSION['buchung_step'] == 2) {
+            include dirname(plugin_dir_path(__FILE__)) . '/templates/step-two.php';
+        } else {
+            include dirname(plugin_dir_path(__FILE__)) . '/templates/step-one.php';
+        }
 
         return ob_get_clean();
     }
