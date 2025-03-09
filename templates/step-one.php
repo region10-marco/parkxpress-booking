f264eab952ddad392a5c34e4ab935d9208c30e87
region10-marco <marco.hinkelmann@region10.group>
2025-03-09 16:38:44 +0100

    Füge das Template für die erste Buchungsstufe hinzu

---
+++ templates/step-one.php
@@ -0,0 +1,21 @@
+<div id="checkDateWrapper">
+    <h1><?php echo $headline; ?></h1>
+    <div id="error" style="display:none;">
+        <p id="errorincontent"></p>
+    </div>
+    <div id="checkDate">
+        <div class="formInput">
+            <label for="check-in"><?php echo $checkinlabel; ?></label>
+            <input type="text" id="check-in" name="check-in" class="textInput" readonly>
+        </div>
+
+        <div class="formInput">
+            <label for="check-out"><?php echo $checkoutlabel; ?></label>
+            <input type="text" id="check-out" name="check-out" class="textInput" readonly>
+        </div>
+
+        <button id="submit" class="checkDateBtn"><?php echo $order_btn_1; ?></button>
+
+        <div class="clear"></div>
+
+</div>
