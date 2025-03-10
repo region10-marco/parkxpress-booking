b10037caff33f209c8499834b4c4b9b2667fdc87
region10-marco <marco.hinkelmann@region10.group>
2025-03-10 07:55:18 +0100

    Aktualisiere das Datumsauswahl-Template mit dynamischen Überschriften und verbessere die Benutzeroberfläche

--- templates/get-slot.php
+++ templates/get-slot.php
@@ -1,27 +1,29 @@
-<div id="stepTwoContainer">
+<div id="checkDateWrapper">
+    <h1><?php echo $headline; ?></h1>
+    <div id="error" style="display:none;">
+        <p id="errorincontent"></p>
+    </div>
     <div id="checkDate">
-        <h1>Wählen Sie Ihr An- und Abreisedatum</h1>
-        <div id="error" style="display:none;">
-            <p id="errorincontent"></p>
-        </div>
+
         <div class="formInput">
-            <label for="check-in">Check-in:</label>
+            <label for="check-in"><?php echo $checkinlabel; ?></label>
             <input type="text" id="check-in" name="check-in" class="textInput" readonly>
         </div>
 
         <div class="formInput">
-            <label for="check-out">Check-out:</label>
+            <label for="check-out"><?php echo $checkoutlabel; ?></label>
             <input type="text" id="check-out" name="check-out" class="textInput" readonly>
         </div>
 
-        <button id="submitDate" class="checkDateBtn">JETZT PRÜFEN</button>
+        <button id="submitDate" class="checkDateBtn"><?php echo $checkbtn; ?></button>
     </div>
 
     <div id="parking-options" style="display:none;">
-        <h1>Wählen Sie Ihren Parkplatz</h1>
+        <h1><?php echo $order_headline; ?></h1>
         <div id="error-parking" style="display:none;">
             <p id="errorincontent"></p>
         </div>
+
         <div id="parking-container"></div>
     </div>
 </div>
